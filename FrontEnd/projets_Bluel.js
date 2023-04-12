ae0243762099a733ba863ac37ccc9e269f39d37c
Alexandre Lerch <alexandre.lerch@yahoo.fr>
2023-04-12 13:56:03 +0200

    PAGE OK

--- FrontEnd/projets_Bluel.js
+++ FrontEnd/projets_Bluel.js
@@ -58,3 +58,212 @@ for (let i = 0 ; i < cat.length; i++) {
         document.querySelector(".filtres").appendChild(bouton);
 }
 //////FIN DU CODE CORRESPONDANT AUX FILTRES DES CATEGORIES
+
+////////EFFACEMENT DES FILTRES SI CONNECTRION OK/////////////
+if(sessionStorage.getItem('adminToken')) {
+    const boxFiltres= document.querySelector(".filtres")
+       boxFiltres.style = "display:none"
+   
+       const affichage = document.querySelectorAll(".iflogged")
+       affichage.forEach(a => {a.setAttribute("style", null)})
+   } else {console.log("false")}
+////////FIN DE EFFACEMENT DES FILTRES SI CONNECTRION OK/////////////
+
+
+
+
+OU
+let modal = null
+
+//OUVERTURE DE LA MODALE AU CLICK DU BOUTON MODIFIER
+const openModal = function(e) {
+    e.preventDefault()
+    const target = document.querySelector('#modal1')
+    target.style.display = null
+    target.setAttribute('aria-hidden', false)
+    target.setAttribute('aria-modal', true)
+    modal = target
+    modal.addEventListener('click', closeModal)
+    modal.querySelector('.js-modal-close').addEventListener('click', closeModal)
+    modal.querySelector('.js-modal-stop').addEventListener('click', stopPropagation)
+}
+
+//OUVERTURE DE LA MODALE AU CLICK SUR LA CROIX
+const closeModal = function (e) {
+    console.log("close")
+    if (modal === null) return
+    e.preventDefault()
+    modal.style.display = 'none'
+    modal.setAttribute('aria-hidden', 'true')
+    modal.removeAttribute('aria-modal')
+    modal.removeEventListener('click', closeModal)
+    modal.querySelector('.js-modal-close').removeEventListener('click', closeModal)
+    modal.querySelector('.js-modal-stop').removeEventListener('click', stopPropagation)
+    modal = null
+}
+
+const stopPropagation = function (e) {
+e.stopPropagation()
+}
+
+// Récupération de l'élément du DOM qui accueillera le bouton
+
+//Alimentation de la modale
+let reponse = await fetch("http://localhost:5678/api/works");
+let projets = await reponse.json();
+
+function genererProjetsModal(projets) {
+    for (let i = 0 ; i < projets.length; i++) {
+        const projet = projets[i];
+     
+        // Récupération de l'élément du DOM qui accueillera figures
+        const galleryModal = document.querySelector(".gallery_modal");
+
+        // Création d’une balise dédiée au projet
+        const figure = document.createElement("figure");
+        figure.id = "fig" + projet.id
+        figure.className = "figures"
+        galleryModal.appendChild(figure);
+                
+        // Création des balises 
+        const image = document.createElement("img");
+        image.src = projet.imageUrl;
+        figure.appendChild(image)
+
+        const figcaption = document.createElement("figcaption");
+        figcaption.innerText = "editer";
+        figure.appendChild(figcaption);
+        
+        const suppression = document.createElement("button");
+        suppression.className = "boutonSuppression"
+        suppression.id = projet.id
+        suppression.type = "submit" 
+        suppression.addEventListener('click', async function() {
+            const id = projet.id
+            await fetch(`http://localhost:5678/api/works/${id}`, {
+                method: "DELETE",
+                headers: {
+                    "Content-Type": "application/json",
+                    "Authorization": `Bearer ${sessionStorage.adminToken}`,
+            }})
+            // .then(document.querySelector(".gallery_modal").innerHTML="")
+            .then(document.querySelector(".gallery_modal").removeChild(document.querySelector("#fig" + id)))
+            .catch(err => console.log(err))
+        })
+
+
+        figure.appendChild(suppression);
+
+        const iconSuppression = document.createElement("i");
+        iconSuppression.className = "fa-solid fa-trash-can"
+        //iconSuppression.src = "assets/icons/trash-can-solid.svg"
+        suppression.appendChild(iconSuppression)
+        
+        //problème à voir avec le bouton deplacement
+        function genererBoutonDeplacement() {
+            const boutonDeplacement = document.createElement("button");
+            boutonDeplacement.className = "boutonDeplacement"
+            //boutonDeplacement.id = i +1
+            //boutonDeplacement.style = "display:none"
+            
+            figure.appendChild(boutonDeplacement);
+            /*figure.addEventListener("mouseover", function() {
+                if(boutonDeplacement.id === figure.id) {
+                boutonDeplacement.style = "display:block"
+                }
+            })
+               */
+            const iconDeplacement = document.createElement("i");
+            iconDeplacement.className = "fa-solid fa-arrows-up-down-left-right"
+            boutonDeplacement.appendChild(iconDeplacement)
+        }
+        genererBoutonDeplacement()
+    }
+ 
+} 
+
+// premier affichage de la page
+document.querySelector(".js-modal").addEventListener('click', openModal, genererProjetsModal(projets))
+
+
+
+//OUVERTURE DE LA GALERIE DE LA MODALE
+const modal1 = document.querySelector("#modal1")
+const modal2 = document.querySelector("#modal2")
+const ajout = document.querySelector(".ajout")
+ajout.addEventListener("click", function(){
+    //modal1.style = "display:none"
+    modal2.style = "display:flex"
+})
+
+//DEFINISSION DES ACTIONS POUR RETURN & CLOSE
+document.querySelector(".js-modal-return").addEventListener("click", function () {
+    modal1.style="display:flex"
+    modal2.style = "display:none"
+})
+document.querySelector(".js-modal-close2").addEventListener("click", function () {
+    modal2.style="display:none"
+})
+
+//RECUPERATION DES CATEGORIES & DU TOKEN VIA LE LOCALHOST
+const categories = await fetch("http://localhost:5678/api/categories")
+const cat = await categories.json();
+const adminToken = sessionStorage.getItem('adminToken')
+
+//CREATION DE LA FONCTION POUR AJOUTER LES FIGURES
+const formulaire = document.querySelector("#formulaire_ajout_figure")
+
+
+let inputFile = document.querySelector("#ajout_image")
+inputFile.classList.add("fileInput");
+inputFile.setAttribute("accept",".png, .jpg, .jpeg");
+inputFile.type= "file";
+
+async function ajoutFigure () {
+
+    // RECUPERATION DES DONNEES DE L'IMAGE    
+
+    // récupération du title
+    const titleValue = document.querySelector("#title").value
+    let categoryValue = document.querySelector("#category").value;
+    //récupération de l'url de l'image
+    let inputFile = document.querySelector("#ajout_image")
+    const file = inputFile.files[0]
+
+    if (titleValue && file) {
+        } else {alert("Formulaire incomplet")
+    }
+
+    //transformation de la valeur en number
+    function roughScale(x, base) {
+        const parsed = parseInt(x, base);
+        if (isNaN(parsed)) {
+            return 0
+        } else {
+            return parsed;
+        }     
+    }
+    let categoryValueInt = roughScale(categoryValue, 10)
+
+    //CREATION DU FORMDATA
+    let formData = new FormData();
+    formData.append('title', titleValue)
+    formData.append('image', file)
+    formData.append('category', categoryValueInt)
+
+    if (formData) {
+        await fetch('http://localhost:5678/api/works', {
+            method: 'POST',
+            headers: {
+                'Authorization':`Bearer ${adminToken}`,
+                'accept': 'application/json',
+            },
+            body: formData
+        })
+    }
+}
+
+formulaire.addEventListener("submit", function(event) {
+    event.preventDefault()
+    ajoutFigure()
+}) 
